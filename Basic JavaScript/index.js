c6d7b00fff835949f12ff1aa6a5b4a0abc24f8a9
yukhimich <145339550+yukhimich@users.noreply.github.com>
2023-12-01 15:10:20 +0200

    Add files via upload

---
+++ Basic JavaScript/index.js
@@ -0,0 +1,11 @@
+document.querySelector('.frame-text02').onclick = function() {
+   window.location.href = 'login.html';
+ };
+ 
+ document.querySelector('.frame-text04').onclick = function() {
+   window.location.href = 'basket.html';
+ };
+ document.querySelector('.frame-cut-store11').onclick = function() {
+   window.location.href = 'index.html';
+ };
+  
